7a6c0a3ed182cdf3264ad6d79902d6830e9ae5af
wassimbedir <wassimbedir60@gmail.com>
2026-09-02 21:21:27 +0100

    Fix exception handling for UniqueViolation error

--- api.py
+++ api.py
@@ -254,7 +254,9 @@ def register():
             }
         }), 201
 
-        except psycopg2.errors.UniqueViolation:
+
+                         except
+    psycopg2.errors.UniqueViolation:
 
         if 'conn' in locals():
             conn.rollback()
